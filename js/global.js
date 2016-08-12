9a401b98b696327a02a62def29ace8532a1af49b
luis <luis.evo_8.10@hotmail.com>
2016-08-12 14:29:27 -0500

    cambios estilos

--- js/global.js
+++ js/global.js
@@ -1,38 +1,11 @@
-angular.module('apirest', [
-        'apirest.SeguridadTranquera','ngRoute'
-    ])
-
-    .config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider.
-                when('/registro', {
-                    templateUrl: 'templates/registro.html'
-                }).
-                otherwise({
-                    redirectTo: '/registro'
-                });
-        }])
-
-    .controller('GlobalController', ['rest_router','$scope', function(rest_router,$scope) {
-        $scope.templates = rest_router.urlList
-        rest_router.getUrl('registro',function(template){
-            $scope.template = template
-        })
-    }])
-
-    .factory("rest_router", function(){
-        this.urlList = 
-            [
-                { name: 'registro', url: '/templates/registro.html'},
-                { name: 'edicion', url: '/templates/edicion.html'}
-            ],
-        this.getUrl = function(name, callback){
-            angular.forEach(this.urlList,function(value, key){
-                //console.log(String(value.name),String(name))
-                if (String(value.name) == String(name)) {
-                    return callback(value)
-                }
-            })
-        }
-        return this;
-   })
+angular
+    .module('apirest', [])
+    // .controller('GlobalController', ['rest_router','$scope', function(rest_router,$scope) {
+    //     $scope.templates = rest_router.urlList
+    //     rest_router.getUrl('registro',function(template){
+    //         $scope.template = template
+    //     })
+    // }])
+    .controller('ControllerApiRest', function () {
+       console.log('En linea')
+    });
